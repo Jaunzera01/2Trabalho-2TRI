cb90dbeb0ac3fcea60ae47c88466978bd3b7e796
João Pedro <89290727+Jaunzera01@users.noreply.github.com>
2024-08-20 10:19:07 -0300

    aaaa

--- script.js
+++ script.js
@@ -9,22 +9,12 @@ const alternativa  = [
         enunciado: "No âmbito social, você prefere:",
         alternativas: [
             {
-<<<<<<< HEAD
                 texto: "Você realiza todos os sonhos da sua vida, porém serão realizados longe de seus amados e eles não poderão saber que foram realizados.",
                 afirmação: "afirmação",
-=======
-                texto: "Você realiza todos os sonhos da sua vida, porém serão realizados longe de seus amados e eles não poderão saber que foram realizados.",
-                afirmacao: "afirmação",
->>>>>>> c7fdcd2a01a9377fd5799f23867fee035429f00f
             },
             {
-<<<<<<< HEAD
                 texto: "Você prefere encontrar a cura para o câncer, porém você tem que escolher uma doença de um familiar seu para nunca ser curada.",
                 afirmação: "afirmação",
-=======
-                texto: "Você prefere encontrar a cura para o câncer, porém você tem que escolher uma doença de um familiar seu para nunca ser curada.",
-                afirmacao: "afirmação",
->>>>>>> c7fdcd2a01a9377fd5799f23867fee035429f00f
 
             },
         ]
@@ -33,22 +23,12 @@ const alternativa  = [
         enunciado: "No âmbito ambiental, você prefere:",
         alternativas: [
             {
-<<<<<<< HEAD
                 texto: "Você prefere despoluir todos os mares e rios, porém terá que escolher uma espécie animal para desaparecer. ",
                 afirmação: "afirmação",
-=======
-                texto: "Você prefere despolui todos os mares e rios, porém terá que escolher uma espécie animal para desaparecer. ",
-                afirmacao: "afirmação",
->>>>>>> c7fdcd2a01a9377fd5799f23867fee035429f00f
             },
             {
-<<<<<<< HEAD
                 texto: "Você prefere melhorar toda a infraestrutura da África, mas a má infraestrutura seria distribuída para outros países de acordo com o seu nível de desenvolvimento.",
                 afirmação: "afirmação",
-=======
-                texto: "Você prefere melhorar toda a infraestrutura da África, mas a má infraestrutura seria distribuída para outros países de acordo com o seu nível de desenvolvimento.",
-                afirmacao: "afirmação",
->>>>>>> c7fdcd2a01a9377fd5799f23867fee035429f00f
 
             },
         ]
@@ -58,22 +38,12 @@ const alternativa  = [
         alternativas: [
 
             {
-<<<<<<< HEAD
                 texto: "Você acabaria com a Deepweb mesmo sabendo que a pessoa que acabou com ela usará toda a informação a favor dela.",
                 afirmação: "afirmação",
-=======
-                texto: "Você acabaria com a Deepweb mesmo sabendo que a pessoa que acabou com ela usará toda a informação a favor dela.",
-                afirmacao: "afirmação",
->>>>>>> c7fdcd2a01a9377fd5799f23867fee035429f00f
             },
             {
-<<<<<<< HEAD
                 texto: "Você prefere expor todos os segredos das maiores empresas do mundo, mas a sociedade entraria em colapso.",
                 afirmação: "afirmação",
-=======
-                texto: "Você prefere expor todos os segredos das maiores empresas do mundo, mas a sociedade entraria em colapso.",
-                afirmacao: "afirmação",
->>>>>>> c7fdcd2a01a9377fd5799f23867fee035429f00f
 
             },
         ]
@@ -82,7 +52,13 @@ const alternativa  = [
 
 let atual = 0
 let perguntaAtual;
+let historiaFinal = "";
 function mostraPerguntas() {
+    if(atual >= perguntas.lenght){
+        
+        return;
+    }
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent= "";
@@ -92,30 +68,17 @@ function mostraAlternativas (){
     for( const Alternativa of  perguntaAtual.alternativas){ 
         const botaoAlternativa=document.createElement( "button");
         botaoAlternativa.textContent=Alternativa.texto;
-<<<<<<< HEAD
         botaoAlternativa.addEventListener("click", () => respoataSelecionada(perguntas)){
             atual++;
             mostraPerguntas();
         })
-=======
-        botaoAlternativa.addEventListener("click", () => respoataSelecionada(Alternativa ));       
->>>>>>> c7fdcd2a01a9377fd5799f23867fee035429f00f
         caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
-<<<<<<< HEAD
 
 function respoataSelecionada(perguntas){
 
     atual++;
-    mostraPerguntas
+    mostraPerguntas;
 }
-=======
-
-function respoataSelecionada(Alternativa ){
-    const afimacoes = Alternativa.afirmacao;
-    atual++;
-    mostraPergunta ();
-}
->>>>>>> c7fdcd2a01a9377fd5799f23867fee035429f00f
 mostraPerguntas();
