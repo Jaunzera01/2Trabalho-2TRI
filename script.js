73917d0ad77911a46bc67184de39d93cff18fd33
João Pedro <89290727+Jaunzera01@users.noreply.github.com>
2024-08-20 09:28:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Você realiza todos os sonhos da sua vida, porém serão realizados longe de seus amados e eles não poderão saber que foram realizados.",
-                afirmação: "afirmação",
+                afirmacao: "afirmação",
             },
             {
                 texto: "Você prefere encontrar a cura para o câncer, porém você tem que escolher uma doença de um familiar seu para nunca ser curada.",
-                afirmação: "afirmação",
+                afirmacao: "afirmação",
 
             },
         ]
@@ -24,11 +24,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Você prefere despolui todos os mares e rios, porém terá que escolher uma espécie animal para desaparecer. ",
-                afirmação: "afirmação",
+                afirmacao: "afirmação",
             },
             {
                 texto: "Você prefere melhorar toda a infraestrutura da África, mas a má infraestrutura seria distribuída para outros países de acordo com o seu nível de desenvolvimento.",
-                afirmação: "afirmação",
+                afirmacao: "afirmação",
 
             },
         ]
@@ -39,11 +39,11 @@ const perguntas = [
 
             {
                 texto: "Você acabaria com a Deepweb mesmo sabendo que a pessoa que acabou com ela usará toda a informação a favor dela.",
-                afirmação: "afirmação",
+                afirmacao: "afirmação",
             },
             {
                 texto: "Você prefere expor todos os segredos das maiores empresas do mundo, mas a sociedade entraria em colapso.",
-                afirmação: "afirmação",
+                afirmacao: "afirmação",
 
             },
         ]
@@ -71,7 +71,7 @@ function mostraAlternativas (){
 }
 
 function respoataSelecionada(pergunta){
-
+    const afimacoes = pergunta.afirmacao;
     atual++;
     mostraPergunta ();
 }
